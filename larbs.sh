3722daada06336c81e9a47898e7422e0a373bc31
Luis <elwolf6@protonmail.com>
2023-01-20 15:36:16 -0800

    merge upstream but not really fuck hugo

--- larbs.sh
+++ larbs.sh
@@ -167,7 +167,8 @@ Include = /etc/pacman.d/mirrorlist-arch" >>/etc/pacman.conf
 manualinstall() {
 	# Installs $1 manually. Used only for AUR helper here.
 	# Should be run after repodir is created and var is set.
-	whiptail --infobox "Installing \"$1\", an AUR helper..." 7 50
+	pacman -Qq "$1" && return 0
+	whiptail --infobox "Installing \"$1\" manually." 7 50
 	sudo -u "$name" mkdir -p "$repodir/$1"
 	sudo -u "$name" git -C "$repodir" clone --depth 1 --single-branch \
 		--no-tags -q "https://aur.archlinux.org/$1.git" "$repodir/$1" ||
@@ -258,6 +259,36 @@ vimplugininstall() {
 	sudo -u "$name" nvim -c "PlugInstall|q|q"
 }
 
+makeuserjs(){
+	arkenfox="$pdir/arkenfox.js"
+	larbs="/home/$name/.config/firefox/larbs.js"
+	userjs="$pdir/user.js"
+	[ ! -f "$arkenfox" ] && curl -sL "https://raw.githubusercontent.com/arkenfox/user.js/master/user.js" > "$arkenfox"
+	cat "$arkenfox" "$larbs" > "$userjs"
+	chown "$name:wheel" "$arkenfox" "$userjs"
+}
+
+installffaddons(){
+	addontmp="$(mktemp -d)"
+	trap "rm -fr $addontmp" HUP INT QUIT TERM PWR EXIT
+	IFS='
+'
+	sudo -u "$name" mkdir -p "$pdir/extensions/"
+	for addon in $addonlist; do
+		file="${addon##*/}"
+		sudo -u "$name" curl -LOs "$addon" > "$addontmp/$file"
+		id="$(unzip -p "$file" manifest.json | grep "\"id\"")"
+		id="${id%\"*}"
+		id="${id##*\"}"
+		sudo -u "$name" mv "$file" "$pdir/extensions/$id.xpi"
+	done
+	# Fix a Vim Vixen bug with dark mode not fixed on upstream:
+	sudo -u "$name" mkdir -p "$pdir/chrome"
+	[ ! -f  "$pdir/chrome/userContent.css" ] && sudo -u "$name" echo ".vimvixen-console-frame {
+  color-scheme: light !important;
+}" > "$pdir/chrome/userContent.css"
+}
+
 finalize() {
 	whiptail --title "All done!" \
 		--msgbox "Congrats! Provided there were no hidden errors, the script completed successfully and all the programs and configuration files should be in place.\\n\\nTo run the new graphical environment, log out and log back in as your new user, then run the command \"startx\" to start the graphical environment (it will start automatically in tty1).\\n\\n.t Luke" 13 80
@@ -301,7 +332,7 @@ done
 
 whiptail --title "LARBS Installation" \
 	--infobox "Synchronizing system time to ensure successful and secure installation of software..." 8 70
-ntpdate 0.us.pool.ntp.org >/dev/null 2>&1
+ntpd -q -g >/dev/null 2>&1
 
 adduserandpass || error "Error adding username and/or password."
 
@@ -350,6 +381,33 @@ dbus-uuidgen >/var/lib/dbus/machine-id
 
 # Use system notifications for Brave on Artix
 echo "export \$(dbus-launch)" >/etc/profile.d/dbus.sh
+
+# All this below to get Pulse installed with add-ons and non-bad settings.
+
+whiptail --infobox "Setting browser privacy settings and add-ons..." 7 60
+
+addonlist="https://addons.mozilla.org/firefox/downloads/file/3929378/ublock_origin-1.42.0-an+fx.xpi
+https://addons.mozilla.org/firefox/downloads/file/3902154/decentraleyes-2.0.17.xpi
+https://addons.mozilla.org/firefox/downloads/file/4035245/istilldontcareaboutcookies-1.1.0.xpi
+https://addons.mozilla.org/firefox/downloads/file/4053589/darkreader-4.9.62.xpi
+https://addons.mozilla.org/firefox/downloads/file/4030629/tampermonkey-4.18.1.xpi
+https://addons.mozilla.org/firefox/downloads/file/4008928/saucenao-69.xpi"
+
+browserdir="/home/$name/.librewolf"
+profilesini="$browserdir/profiles.ini"
+
+# Start librewolf headless so it generates a profile. Then get that profile in a variable.
+sudo -u "$name" librewolf --headless >/dev/null 2>&1 &
+sleep 1
+profile="$(sed -n "/Default=.*.default-release/ s/.*=//p" "$profilesini")"
+pdir="$browserdir/$profile"
+
+[ -d "$pdir" ] && makeuserjs
+
+[ -d "$pdir" ] && installffaddons
+
+# Kill the now unnecessary librewolf instance.
+pkill -u "$name" librewolf
 
 # Allow wheel users to sudo with password and allow several system commands
 # (like `shutdown` to run without password).
