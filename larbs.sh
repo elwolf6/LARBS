4273103065550a6fa8d70757a67461e7342f1f45
elwolf <elwolf6@protonmail.com>
2023-01-22 00:26:35 -0500

    Update larbs.sh

--- larbs.sh
+++ larbs.sh
@@ -362,6 +362,7 @@ sudo -u "$name" mkdir -p "/home/$name/qwertyuiop-server/"
 
 rm -rf "/home/$name/go"
 
+chown -R $name /etc/spoitfy
 spicetify restore
 spicetify backup apply
 curl -fsSL https://raw.githubusercontent.com/Tetrax-10/Nord-Spotify/master/install-scripts/install.sh | sh
